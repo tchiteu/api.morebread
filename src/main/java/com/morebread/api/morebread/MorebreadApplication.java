1abc8ab1ea0b1612a8e40ced69706f7a7cb3a6a1
tchiteu <matheussantos.contato362@gmail.com>
2020-08-23 01:53:17 -0300

    Primeiro commit :zap:

---
+++ src/main/java/com/morebread/api/morebread/MorebreadApplication.java
@@ -0,0 +1,13 @@
+package com.morebread.api.morebread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorebreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorebreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morebread/api/morebread/MorebreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morebread.api.morebread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorebreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
