783236f8eb10fc2eda17323cde9c396db07d2eb1
tchiteu <matheussantos.contato362@gmail.com>
2020-08-23 02:35:37 -0300

    Criando model de usuários

---
+++ src/main/java/com/morebread/api/morebread/model/Usuario.java
@@ -0,0 +1,57 @@
+package com.morebread.api.morebread.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Usuario {
+	private Long id;
+  private String nome;
+  private String email;
+	private String cargo;
+	private String senha;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setCargo(String cargo) {
+    this.cargo = cargo;
+  }
+
+  public String getCargo() {
+    return this.cargo;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  public String getSenha() {
+    return this.senha;
+  }
+}
