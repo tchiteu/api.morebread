5bbc4f11387589534bb770af107ee371a0f33524
tchiteu <matheussantos.contato362@gmail.com>
2020-08-27 21:12:54 -0300

    Finalizando repository

---
+++ src/main/java/com/morebread/api/morebread/model/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.morebread.api.morebread.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
